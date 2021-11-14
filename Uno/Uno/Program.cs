4631e7bc6ee1d8f941405b6c3ec35643f2986a81
Toast <TotaledToast@gmail.com>
2021-11-14 00:24:19 +0000

    +2 Works Now, Need to Fix Reverse

--- Uno/Uno/Program.cs
+++ Uno/Uno/Program.cs
@@ -11,26 +11,47 @@ namespace Open_Evening
             List<Card> deck1 = new List<Card>();
             List<Card> deck2 = new List<Card>();
             Card LastCardPlayed = new Card();
+            int Plus = 0;
             LastCardPlayed.number = 0;
             LastCardPlayed.color = ConsoleColor.Green;
             GenerateHand(deck1);
             Console.WriteLine("------------------------");
             GenerateHand(deck2);
-            while (deck1.Count != 0 || deck2.Count != 0)
+            Console.WriteLine(deck1.Count);
+            while (deck1.Count != 0 && deck2.Count != 0)
             {
-                PlayRound(deck1, LastCardPlayed, 1);
-                PlayRound(deck2, LastCardPlayed, 2);
+                Plus = PlayRound(deck1, LastCardPlayed, 1, Plus);
+                Console.WriteLine(Plus);
+                Console.ReadLine();
+                Plus = PlayRound(deck2, LastCardPlayed, 2, Plus);
+                Console.WriteLine(Plus);
+                Console.ReadLine();
+            }
+            if (deck1.Count == 0)
+            {
+                Console.WriteLine("Congrats on winning Player1");
+            }
+            else
+            {
+                Console.WriteLine("Congrats on winning Player2");
             }
         }
 
-        static void PlayRound(List<Card> deck, Card LastCardPlayed, int Player)
+        static int PlayRound(List<Card> deck, Card LastCardPlayed, int Player, int Plus)
         {
             Console.Clear();
             int Counter = 0, PickedCard;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("--Last Card Played--");
             Console.ForegroundColor = LastCardPlayed.color;
-            Console.WriteLine(LastCardPlayed.number);
+            if (LastCardPlayed.number <= 9)
+            {
+                Console.WriteLine(LastCardPlayed.number);
+            }
+            else
+            {
+                Console.WriteLine(LastCardPlayed.specialType);
+            }
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\n");
             Console.WriteLine("--Player" + Player + "'s Deck--");
@@ -40,11 +61,15 @@ namespace Open_Evening
                 Console.ForegroundColor = item.color;
                 if (item.number  <= 9)
                 {
-                    Console.WriteLine(item.number + "\t ----------  " + Counter);
+                    Console.WriteLine(item.number + "\t\t ----------  " + Counter);
+                }
+                else if (item.number == 14)
+                {
+                    Console.WriteLine(item.specialType + "\t ----------  " + Counter);
                 }
                 else
                 {
-                    Console.WriteLine(item.specialType + "\t ----------  " + Counter);
+                    Console.WriteLine(item.specialType + "\t\t ----------  " + Counter);
                 }
                 Console.ForegroundColor = ConsoleColor.White;
                 Counter++;
@@ -56,22 +81,29 @@ namespace Open_Evening
             {
                 if (item.PlaceInDeck == PickedCard)
                 {
-                    Fight(LastCardPlayed, item, deck);
+                    Plus = Fight(LastCardPlayed, item, deck, Plus);
                     break;
                 }
             }
-
+            return Plus;
         }
 
-        static void Fight(Card card1, Card card2, List<Card> deck)
+        static int Fight(Card card1, Card card2, List<Card> deck, int Plus)
         {
-            if (card2.specialType == "Plus 4" || card2.specialType == "Pick Color")
+            if (Plus > 0)
             {
                 switch (card2.number)
                 {
-                    case 11:
+                    case 10:
+                        Plus = Plus + 2;
+                        card1.number = card2.number;
+                        card1.color = card2.color;
+                        card1.specialType = card2.specialType;
+                        card1.cardType = card2.cardType;
+                        deck.Remove(card2);
                         break;
-                    case 14:
+                    case 11:
+                        Plus = Plus + 4;
                         int color;
                         Console.WriteLine("What should the new color be?");
                         Console.WriteLine("GREEN\t ------- 0");
@@ -79,30 +111,98 @@ namespace Open_Evening
                         Console.WriteLine("BLUE\t ------- 2");
                         Console.WriteLine("YELLOW\t ------- 3");
                         color = Convert.ToInt32(Console.ReadLine());
-                        switch(color)
+                        switch (color)
                         {
                             case 0:
                                 card1.color = ConsoleColor.Green;
+                                card1.specialType = "GREEN";
                                 break;
                             case 1:
                                 card1.color = ConsoleColor.Red;
+                                card1.specialType = "RED";
                                 break;
                             case 2:
                                 card1.color = ConsoleColor.Blue;
+                                card1.specialType = "BLUE";
                                 break;
                             case 3:
                                 card1.color = ConsoleColor.Yellow;
+                                card1.specialType = "YELLOW";
                                 break;
                         }
-                        card1.number = -1;
-                        card1.specialType = "";
-                        card1.cardType = Open_Evening.Card.CardType.Number;
+                        card1.number = card2.number;
+                        card1.cardType = card2.cardType;
+                        deck.Remove(card2);
+                        break;
+                    default:
+                        for (int i = 0; i < Plus; i++)
+                        {
+                            GenerateCard(deck);
+                        }
+                        card1.number = 0;
+                        Plus = 0;
                         break;
                 }
+            }
+            else if (card2.specialType == "Plus 4" || card2.specialType == "Pick Color")
+            {
+                switch (card2.number)
+                {
+                    case 11:
+                        Plus = Plus + 4;
+                        break;
+                    case 14:
+                        break;
+                }
+                int color;
+                Console.WriteLine("What should the new color be?");
+                Console.WriteLine("GREEN\t ------- 0");
+                Console.WriteLine("RED\t ------- 1");
+                Console.WriteLine("BLUE\t ------- 2");
+                Console.WriteLine("YELLOW\t ------- 3");
+                color = Convert.ToInt32(Console.ReadLine());
+                switch (color)
+                {
+                    case 0:
+                        card1.color = ConsoleColor.Green;
+                        card1.specialType = "GREEN";
+                        break;
+                    case 1:
+                        card1.color = ConsoleColor.Red;
+                        card1.specialType = "RED";
+                        break;
+                    case 2:
+                        card1.color = ConsoleColor.Blue;
+                        card1.specialType = "BLUE";
+                        break;
+                    case 3:
+                        card1.color = ConsoleColor.Yellow;
+                        card1.specialType = "YELLOW";
+                        break;
+                }
+                card1.number = card2.number;
+                card1.cardType = card2.cardType;
+                deck.Remove(card2);
 
             }
             else if (card1.number == card2.number || card1.color == card2.color)
             {
+                switch(card2.number)
+                {
+                    case 10:
+                        Plus = Plus + 2;
+                        break;
+                    case 11:
+                        if(card1.number == 10 || card1.number == 11)
+                        {
+                            Plus = 0;
+                        }
+                        break;
+                    case 12:
+                        break;
+                    default:
+                        break;
+                }
                 card1.number = card2.number;
                 card1.color = card2.color;
                 card1.specialType = card2.specialType;
@@ -114,6 +214,7 @@ namespace Open_Evening
             {
                 GenerateCard(deck);
             }
+            return Plus;
         }
 
         static List<Card> GenerateHand(List<Card> deck)
